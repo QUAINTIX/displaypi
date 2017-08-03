5fe4a7a5dcbab9758c4fd6b071739ea8c1516723
QUAINTIX <hansmann@quaintix.com>
2017-08-03 10:36:14 +0200

    new version

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<div style="border: 1px solid red; margin: auto auto; width: 50%; text-align: center;">
+<div style="border: 1px solid blue; margin: auto auto; width: 50%; text-align: center;">
 	<img src="images/raspberrypizero.png">
 <div>
